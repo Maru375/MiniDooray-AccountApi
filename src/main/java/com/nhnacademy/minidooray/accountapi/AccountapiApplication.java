8777c6d2c62ebb9f7cbb5015705d7b1e4e55d5de
Maru375 <www980304@gmail.com>
2024-02-22 09:46:33 +0900

    feat: first commit

---
+++ src/main/java/com/nhnacademy/minidooray/accountapi/AccountapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.accountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray/accountapi/AccountapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
