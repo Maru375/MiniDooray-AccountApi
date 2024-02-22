19574846b46c69a63c76c977cd7d7f69f8db61ae
Mizz1ove <pa990512@naver.com>
2024-02-22 17:02:44 +0900

    feat: 다시 커밋

--- src/main/java/com/nhnacademy/minidooray/accountapi/repository/MemberStateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.minidooray.accountapi.repository;
-
-import com.nhnacademy.minidooray.accountapi.domain.MemeberState;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberStateRepository extends JpaRepository<MemeberState, String> {
-}
